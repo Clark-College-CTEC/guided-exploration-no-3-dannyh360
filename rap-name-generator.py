7ee6a9e1ec8c641ffec2ec327bcfa0c700edf8d6
Your Name <you@example.com>
2020-11-05 12:12:33 -0800

    grade please-helpful assignment

--- rap-name-generator.py
+++ rap-name-generator.py
@@ -19,7 +19,7 @@ outputFile = open('rap-names-output.txt', 'w')
 with open('rap-names.txt', 'r') as dataFile:
     # COMMENT THIS CODE:for loop that iterates through each line in the text file
     for name in dataFile:
-        # COMMENT THIS CODE:strips of the invsibile line at the end of each line
+        # COMMENT THIS CODE:strips off the invsibile line at the end of each line and appends text to possible_names list
         possible_names.append(name.rstrip())
 # Snippet 5
 # == == == == == == == == == == == == == == == ==
@@ -31,19 +31,19 @@ parts = int(input('How many parts should the name contain? '))
 # == == == == == == == == == == == == == == == ==
 # COMMENT THIS CODE:here is a counted loop that will generate the total number of rap names
 for i in range(count):
-    # COMMENT THIS CODE:this the accumulator for the name parts it is an empty list to store input data
+    # COMMENT THIS CODE:empty list to store input data
     name_parts = []
     # COMMENT THIS CODE:here is for loop that interates through all the parts
     for j in range(parts):
         # COMMENT THIS CODE:appends the names to the names parts list
         name_parts.append(
             possible_names[random.randint(0, len(possible_names)-1)])
-# Snippet 7
-# == == == == == == == == == == == == == == == ==
-# COMMENT THIS CODE:this writes the names parts list and joins them together
-outputFile.write(f"{' '.join(name_parts)}\n")
-# COMMENT THIS CODE:this will print the name parts together
-print(f"{' '.join(name_parts)}")
+        # Snippet 7
+        # == == == == == == == == == == == == == == == ==
+        # COMMENT THIS CODE:joins the elements of the name parts list and turns them into a string
+        outputFile.write(f"{' '.join(name_parts)}\n")
+        # COMMENT THIS CODE:this will print the name parts together
+        print(f"{' '.join(name_parts)}")
 # Snippet 8
 # == == == == == == == == == == == == == == == ==
 # COMMENT THIS CODE:this closes the output file
